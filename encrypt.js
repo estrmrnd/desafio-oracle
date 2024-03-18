edf842830bda7145de7638559cc34e5c7f05e5cd
Ester Miranda <esterssmiranda@gmail.com>
2024-03-17 23:28:01 -0300

    mudança no js com o copy clipboard

--- encrypt.js
+++ encrypt.js
@@ -1,75 +1,25 @@
-const textArea = document.querySelector("#textarea");
-const mensagem = document.querySelector(".mensagem");
-const imgLupa = document.querySelector(".boneco");
-const exibicaoText = document.querySelector("#retangulo");
+var textApresentacao = document.querySelector("#textarea");
+var output = document.querySelector("#output");
 
-// As "chaves" de criptografia que utilizaremos são:
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "a" é convertida para "ai"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
 
-function esconderMensagemDeNaoEncontrado() {
-    boneco.style.display = 'none'
-    exibicaoText.style.display = 'none'
+function criptografar(){
+    var texto = textApresentacao.value;
+
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g,"ufat");
+
+document.getElementById('output').innerHTML = '<textarea readonly id="textarea">' + resultCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function mostrarMensagemDeNaoEncontrado() {
-    boneco.style.display = 'block'
-    exibicaoText.style.display = 'block'
+function descriptografar(){
+    var texto = textApresentacao.value;
+
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="textarea">' + resultDescripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function btnEncriptar() {
-    const textoEncriptado = encriptar(textArea.value);
-    mensagem.value = textoEncriptado;
-    textArea.value = "";
-
-    console.log("botao")
-}
-
-function btnDesencriptar() {
-    const textoDencriptado = desencriptar(textArea.value);
-    mensagem.value = textoDencriptado;
-    textArea.value = "";
-}
-
-function copiarTexto() {
-    let texto = document.querySelector(".mensagem");
-    mensagem.select();
-    document.execCommand("copy");
-    mensagem.value = "";
-    mostrarMensagemDeNaoEncontrado();
-}
-
-function encriptar(stringEncriptada) {
-    let matrizCodigo = [["e" , "enter"] , ["i" ,"ines"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat" ]];
-
-    stringEncriptada = stringEncriptada.toLowerCase();
-
-    for(let i = 0; i < matrizCodigo.length; i++) {
-        if(stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }
-    }
-
-    esconderMensagemDeNaoEncontrado();
-    
-    return stringEncriptada;
-}
-
-function desencriptar(stringDesencriptada) {
-    let matrizCodigo = [["e" , "enter"] , ["i" ,"ines"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat" ]];
-
-    stringDesencriptada = stringDesencriptada.toLowerCase();
-
-    for(let i = 0; i < matrizCodigo.length; i++) {
-        if(stringDesencriptada.includes(matrizCodigo[i][0])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-        }
-    }
-
-    esconderMensagemDeNaoEncontrado();
-
-    return stringDesencriptada;
-}
+function copiar(){
+    let texto = textarea.value;
+    navigator.clipboard.writeText(texto);  
+    alert("Copiado para a área de transferência.");
+ }
